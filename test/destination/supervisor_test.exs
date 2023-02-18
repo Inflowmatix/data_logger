81f3b90f4cd13a43605d7a571b3352bae5080e43
njichev <njichev@gmail.com>
2023-02-18 17:54:37 +0200

    Fix test to original body.
    Forgot that I was checking if the test was failing in the opposite
    scenario.

--- test/destination/supervisor_test.exs
+++ test/destination/supervisor_test.exs
@@ -40,7 +40,7 @@ defmodule DataLogger.Destination.SupervisorTest do
     Process.sleep(1500)
 
     refute Process.alive?(pid1)
-    assert Process.alive?(pid2)
+    refute Process.alive?(pid2)
   end
 
   test "supervises only the processes with the right topic, if prefix is used" do
